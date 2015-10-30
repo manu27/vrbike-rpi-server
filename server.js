57ebe79a79ad2c2573899209231921402b077fff
Manuel Heibel <manuel.heibel@gmx.de>
2015-10-30 20:29:20 +0100

    Add socket.io to server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var io = require('socket.io')(app);
 
 app.use('/dashboard', express.static('dashboard'));
+
+io.on('connection', function (socket) {
+    console.log("Device connected!");
+});
 
 var server = app.listen(3000, function () {
     console.log('Example app listening on port 3000');
