0035a39ec64d1eb6415f02529ab18e146a68326a
Manuel Heibel <manuel.heibel@gmx.de>
2015-10-30 19:40:06 +0100

    Make dashboard reachable via node server (/dashboard)

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var app = express();
-var gpio = require('pi-gpio');
 
-app.get('/', function (req, res) {
-    gpio.open(22, "input", function(err) {
-        gpio.read(22, function (err, value) {
-            if (err) throw err;
-            console.log(value); // The current state of the pin
-            res.send(value);
-        });
-    });
-});
+app.use('/dashboard', express.static('dashboard'));
 
 var server = app.listen(3000, function () {
-
     console.log('Example app listening on port 3000');
 });
