f4df556d70102e39460e01a6435e4aa9e1489898
Manuel Heibel <manuel.heibel@gmx.de>
2015-11-02 19:17:58 +0100

    Implement speed service and config data

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var config = require('config');
+var speedSensor = require('services/speed');
+
+speedSensor.initialize(config.get('speed'));
 
 app.use('/dashboard', express.static('dashboard'));
 
 io.on('connection', function (socket) {
     console.log("Device connected!");
+
+    socket.interval = setInterval(function () {
+        socket.emit('speed', {
+            kmPerHour: speedSensor.getCurrentSpeed()
+        });
+    }, 100);
 });
 
 var server = http.listen(3000, function () {
---
+++ services/speed.js
@@ -0,0 +1,48 @@
+var gpio = require('gpio');
+
+/**
+ * var options = {
+ *  radius: 0.2,
+ *  numberOfMagnets: 4,
+ *  scale: 1
+ * }
+ */
+var gpio25, frequency, options;
+
+module.exports = {
+
+    initialize: function (settings) {
+        options = settings;
+
+        gpio25 = gpio.export(25, {
+            direction: 'in',
+            interval: 10,
+            ready: function () {
+                var time;
+                console.log("GPIO25 ready to read!");
+
+                gpio25.on('change', function (val) {
+                    if (time) {
+                        var diff = process.hrtime(time);
+                        frequency = (diff[0] * 1e9 + diff[1]) / 1e6;
+                        console.log(frequency);
+                    }
+                    time = process.hrtime();
+                });
+            }
+        });
+    },
+
+    getCurrentSpeed: function () {
+        if (!frequency) {
+            return 0;
+        }
+
+        var circumference = 2 * Math.PI * options.radius;
+        var mToMDistance = circumference / options.numberMagnets; // Magnet-to-magnet distance
+        var meterPerSecond = 1000 / frequency * mToMDistance * options.scale;
+
+        return meterPerSecond * 3.6; // Output in km/h
+    }
+
+};
